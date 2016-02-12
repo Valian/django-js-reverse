dcdeb5e9a915af7cf7f9ae58fb6f8c798cbceef8
Valian <jakub.skalecki@gmail.com>
2016-02-12 13:01:06 +0100

    option to change destination path

--- django_js_reverse/management/commands/collectstatic_js_reverse.py
+++ django_js_reverse/management/commands/collectstatic_js_reverse.py
@@ -15,13 +15,23 @@ from django_js_reverse.core import generate_js
 class Command(BaseCommand):
     help = 'Creates a static urls-js file for django-js-reverse'
 
-    def handle(self, *args, **options):
-        if not hasattr(settings, 'STATIC_ROOT') or not settings.STATIC_ROOT:
-            raise ImproperlyConfigured('The collectstatic_js_reverse command needs settings.STATIC_ROOT to be set.')
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--directory', '-d', default=None, type=str,
+            help="Define where to store generated file. Defaults to STATIC_ROOT/django_js_reverse/js/")
+        parser.add_argument(
+            '--name', '-n', default='reverse.js', type=str,
+            help="Define name of generated file. Defaults to reverse.js")
 
-        location = os.path.join(settings.STATIC_ROOT, 'django_js_reverse', 'js')
-        file = 'reverse.js'
-        fs = FileSystemStorage(location=location)
+    def handle(self, *args, **options):
+        file = options['name']
+        path = options['directory']
+        if path is None:
+            if not hasattr(settings, 'STATIC_ROOT') or not settings.STATIC_ROOT:
+                raise ImproperlyConfigured('The collectstatic_js_reverse command needs settings.STATIC_ROOT to be set.')
+            path = os.path.join(settings.STATIC_ROOT, 'django_js_reverse', 'js')
+
+        fs = FileSystemStorage(location=path)
         if fs.exists(file):
             fs.delete(file)
 
@@ -29,4 +39,4 @@ class Command(BaseCommand):
         content = generate_js(default_urlresolver)
         fs.save(file, ContentFile(content))
         if len(sys.argv) > 1 and sys.argv[1] in ['collectstatic_js_reverse']:
-            self.stdout.write('js-reverse file written to %s' % (location))  # pragma: no cover
+            self.stdout.write('js-reverse file written to %s' % (os.path.join(path, file)))  # pragma: no cover
